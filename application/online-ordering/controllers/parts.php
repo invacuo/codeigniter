6545b11d40c952c4d3a3c6773646c8c511c57797
Chaitanya Kale <chaitanya.r.kale@gmail.com>
2013-04-23 21:08:16 -0400

    added form validation
    - some minor UI changes
    -disabled query strings for now as codeigniter's form helper does not
    play well with query strings

--- application/online-ordering/controllers/parts.php
+++ application/online-ordering/controllers/parts.php
@@ -11,6 +11,26 @@ class Parts extends CI_Controller {
 		
 		$data['title'] = 'Parts Catalog';		
 		$this->load->view('templates/header', $data);
+		
+		
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+		
+		
+		//Default form validation provided by codeigniter does not work for checkboxes
+		//as nothing is submitted unless a checkbox is checked		
+		//TODO: Extend the form validation library and add a new checked validation to it
+		//$this->form_validation->set_rules('part-id[]', 'Part', 'required'); 
+		
+		if(!empty($_POST)) {
+			if(empty($_POST['part-id'])) {	
+				$data['message'] = 'Please select at least one part.';
+				$this->load->view('pages/flash_message', $data);
+			} else {
+				//add the parts to the cart
+			}
+		}
+		
 		$this->load->view('pages/part_list', $data);
 		$this->load->view('templates/footer');
 	}
--- application/online-ordering/views/pages/part_list.php
+++ application/online-ordering/views/pages/part_list.php
@@ -2,29 +2,32 @@
 if(count($parts)===0) {
 	echo 'There are no parts in the catalog.';
 } else {
+	echo validation_errors();
+	echo form_open('',array('name' => 'part-list-form', 'id' => 'part-list-form'));
+	
 ?>
-	<form name="part-list-form" method="post">
 		<table class="table table-bordered table-hover">
 			<thead>
 				<tr>
-					<th><input type="checkbox" class=".select-all" /></th>
+					<th><?php echo form_checkbox(array('class'=>'.select-all'));?></th>
 					<th>Part Name</th>
 					<th>Part Category</th>
 				</tr>
 			</thead>
+			
+            
 			<tbody>
 				<?php foreach ($parts as $part){ ?>
 			    <tr>
-			    	<td><input type="checkbox" name="part-id[]" value="<?php echo $part['id']?>" /></td>
+			    	<td><?php echo form_checkbox('part-id[]', $part['id'])?></td>
 			    	<td><?php echo $part['name']?></td>
 			    	<td><?php echo $part['part_category']?></td>
-			    	
 				</tr>
 				<?php } ?>
 				<tr>
 					<td colspan="3">
 						<div class="center">
-							<input type="submit" value="Add to cart" class="btn btn-primary"/> 
+							<input type="submit" name="submit" value="Add to cart" class="btn btn-primary"/> 
 							<input type="reset" value="Clear" class="btn" />
 						</div>
 					</td>
