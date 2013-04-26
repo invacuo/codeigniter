3c4cca480dc07f57ff3628d4b5f588a95047db2f
Chaitanya Kale <chaitanya.r.kale@gmail.com>
2013-04-26 17:16:06 -0400

    ci_sessions table for storing sessions in DB

--- create_database.sql
+++ create_database.sql
@@ -128,3 +128,12 @@ VALUES
 COMMIT;
 
 ALTER TABLE  order_details ADD  unit_price DECIMAL NOT NULL DEFAULT  '0'
+
+CREATE TABLE IF NOT EXISTS ci_sessions (
+  session_id varchar(40) NOT NULL DEFAULT '0',
+  ip_address varchar(16) NOT NULL DEFAULT '0',
+  user_agent varchar(250) NOT NULL,
+  last_activity int(10) unsigned NOT NULL DEFAULT '0',
+  user_data longtext NOT NULL,
+  PRIMARY KEY (session_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
